84902fa69e4758c8c6e88f3ec5f9c5b75401cbf3
naramuratomofumi <tomofuminaramura@gmail.com>
2021-12-17 11:16:33 +0900

    バリデーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,13 @@
 class Item < ApplicationRecord
   belongs_to :user
+
+  validates :item_name, presence: true
+  validates :item_text, presence: true
+  validates :category_id, presence: true
+  validates :sales_status_id, presence: true
+  validates :shipping_cost_id, presence: true
+  validates :shipping_area_id, presence: true
+  validates :day_to_ship_id, presence: true
+  validates :price, presence: true
+  validates :image, presence: true
 end
