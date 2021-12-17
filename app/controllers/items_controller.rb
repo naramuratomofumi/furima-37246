f6d6a3873a2e5265611bd3a90f336cbaab5abbd0
naramuratomofumi <tomofuminaramura@gmail.com>
2021-12-17 19:13:05 +0900

    商品出品の設定(DBに保存も確認済み)

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -4,14 +4,20 @@ class ItemsController < ApplicationController
   end
 
   def new
+    @item = Item.new
   end
 
   def create
-    
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
   end
 
   private
   def item_params
-    params.require(:item).permit(:item_name, :item_text, :category_id, :sales_status_id, :sipping_cost_id, :shipping_area_id, :day_to_ship_id, :price, :image).marge(user_id:current_user.id)
+    params.require(:item).permit(:item_name, :item_text, :category_id, :sales_status_id, :shipping_cost_id, :shipping_area_id, :day_to_ship_id, :price, :image).merge(user_id:current_user.id)
   end
 end
