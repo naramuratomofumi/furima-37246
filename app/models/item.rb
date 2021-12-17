740fe67e18d0971835823e175ebc1d04d32a9b98
naramuratomofumi <tomofuminaramura@gmail.com>
2021-12-17 15:32:45 +0900

    発送元の地域のActiveHashを設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,6 +3,7 @@ class Item < ApplicationRecord
   belongs_to :category
   belongs_to :sales_status
   belongs_to :shipping_cost
+  belongs_to :shipping_area
   has_one_attached :image
   
 
@@ -11,7 +12,7 @@ class Item < ApplicationRecord
   validates :category_id, numericality: { other_than: 1 , message: "can't be blank"}
   validates :sales_status_id, presence: true { other_than: 1 , message: "can't be blank"}
   validates :shipping_cost_id, presence: true { other_than: 1 , message: "can't be blank"}
-  validates :shipping_area_id, presence: true
+  validates :shipping_area_id, presence: true{ other_than: 1 , message: "can't be blank"}
   validates :day_to_ship_id, presence: true
   validates :price, presence: true
   validates :image, presence: true
