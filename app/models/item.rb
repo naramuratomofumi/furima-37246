af1b57a6df21971972668b8e59f18fb6c3b9db24
naramuratomofumi <tomofuminaramura@gmail.com>
2021-12-17 19:10:46 +0900

    ActiveHashの記述漏れと修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,5 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :user
   belongs_to :category
   belongs_to :sales_status
@@ -10,10 +11,10 @@ class Item < ApplicationRecord
   validates :item_name, presence: true
   validates :item_text, presence: true
   validates :category_id, numericality: { other_than: 1 , message: "can't be blank"}
-  validates :sales_status_id, presence: true { other_than: 1 , message: "can't be blank"}
-  validates :shipping_cost_id, presence: true { other_than: 1 , message: "can't be blank"}
-  validates :shipping_area_id, presence: true{ other_than: 1 , message: "can't be blank"}
-  validates :day_to_ship_id, presence: true { other_than: 1 , message: "can't be blank"}
+  validates :sales_status_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :shipping_cost_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :shipping_area_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :day_to_ship_id, numericality: { other_than: 1 , message: "can't be blank"}
   validates :price, presence: true
   validates :image, presence: true
 end
