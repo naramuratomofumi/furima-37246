d5675e226c6c616da5ea2f302e5b6bc7e009a26b
naramuratomofumi <tomofuminaramura@gmail.com>
2021-12-17 15:02:49 +0900

    商品状態のActiveHashを追加

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,12 +1,14 @@
 class Item < ApplicationRecord
   belongs_to :user
   belongs_to :category
+  belongs_to :sales_status
   has_one_attached :image
+  
 
   validates :item_name, presence: true
   validates :item_text, presence: true
   validates :category_id, numericality: { other_than: 1 , message: "can't be blank"}
-  validates :sales_status_id, presence: true
+  validates :sales_status_id, presence: true { other_than: 1 , message: "can't be blank"}
   validates :shipping_cost_id, presence: true
   validates :shipping_area_id, presence: true
   validates :day_to_ship_id, presence: true
