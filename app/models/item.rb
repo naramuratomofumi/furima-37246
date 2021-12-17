ffbcd1ab6769437cbd67ea7118027fba7151c772
naramuratomofumi <tomofuminaramura@gmail.com>
2021-12-17 14:53:55 +0900

    カテゴリーのActiveHashを設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,10 +1,11 @@
 class Item < ApplicationRecord
   belongs_to :user
+  belongs_to :category
   has_one_attached :image
 
   validates :item_name, presence: true
   validates :item_text, presence: true
-  validates :category_id, presence: true
+  validates :category_id, numericality: { other_than: 1 , message: "can't be blank"}
   validates :sales_status_id, presence: true
   validates :shipping_cost_id, presence: true
   validates :shipping_area_id, presence: true
