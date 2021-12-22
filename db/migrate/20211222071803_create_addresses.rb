8be56184859ab1242e0ba1c3c64baa71d07b1f33
naramuratomofumi <tomofuminaramura@gmail.com>
2021-12-22 16:33:17 +0900

    orders,addressesテーブルにカラムの追加

--- db/migrate/20211222071803_create_addresses.rb
+++ db/migrate/20211222071803_create_addresses.rb
@@ -1,7 +1,13 @@
 class CreateAddresses < ActiveRecord::Migration[6.0]
   def change
     create_table :addresses do |t|
-
+      t.string     :post_code,          null: false
+      t.integer    :prefectures_id,     null: false
+      t.string     :municipalities,    null: false
+      t.string     :address,            null: false
+      t.string     :building_num
+      t.string     :telephone_num,      null: false
+      t.references :order,              null: false, foreign_key: true
       t.timestamps
     end
   end
