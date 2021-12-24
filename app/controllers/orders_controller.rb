05ce956c776ffc59b94050a9c818046c9f4bb772
naramuratomofumi <tomofuminaramura@gmail.com>
2021-12-24 20:40:24 +0900

    ordersテーブルとaddressesテーブルに一つのフォームから登録できるように設定

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,8 +1,23 @@
 class OrdersController < ApplicationController
   def index
     @item = Item.find(params[:item_id])
+    @order_address = OrderAddress.new
   end
 
   def create
+    @item = Item.find(params[:item_id])
+    @order_address = OrderAddress.new(order_params)
+    if @order_address.valid?
+      @order_address.save
+      redirect_to root_path
+    else
+      render action: :index
+    end
+  end
+
+  private
+
+  def order_params
+    params.require(:order_address).permit(:post_code, :prefectures_id, :municipalities, :address, :building_num, :telephone_num, :item_id).merge(user_id: current_user.id, item_id: @item.id)
   end
 end
---
+++ app/models/order_address.rb
@@ -0,0 +1,19 @@
+class OrderAddress
+  include ActiveModel::Model
+  attr_accessor :post_code, :prefectures_id, :municipalities, :address, :building_num, :telephone_num, :item_id, :token, :user_id
+
+  with_options presence: true do
+    validates :post_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
+    validates :municipalities
+    validates :address
+    validates :telephone_num, format: {with: /\A\d{10}\z|\A\d{11}\z/ , message: "is invalid."}
+  end
+
+  validates :prefectures_id, numericality: { other_than: 1, message: "can't be blank" }
+
+
+  def save
+    order = Order.create(user_id: user_id, item_id: item_id)
+    Address.create(post_code: post_code, prefectures_id: prefectures_id, municipalities: municipalities, address: address, building_num: building_num, telephone_num: telephone_num, order_id: order.id)
+  end
+end
