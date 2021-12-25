e17a719ea0955b31e412ada121f12cee13eb98bb
naramuratomofumi <tomofuminaramura@gmail.com>
2021-12-25 15:52:36 +0900

    クレジット決済機能の実装

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -8,6 +8,7 @@ class OrdersController < ApplicationController
     @item = Item.find(params[:item_id])
     @order_address = OrderAddress.new(order_params)
     if @order_address.valid?
+      pay_item
       @order_address.save
       redirect_to root_path
     else
@@ -18,6 +19,16 @@ class OrdersController < ApplicationController
   private
 
   def order_params
-    params.require(:order_address).permit(:post_code, :prefectures_id, :municipalities, :address, :building_num, :telephone_num, :item_id).merge(user_id: current_user.id, item_id: @item.id)
+    params.require(:order_address).permit(:post_code, :prefectures_id, :municipalities, :address, :building_num, :telephone_num).merge(user_id: current_user.id, item_id: @item.id, token: params[:token])
   end
+
+  def pay_item
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+    Payjp::Charge.create(
+      amount: @item.price,
+      card: order_params[:token],
+      currency:'jpy'
+    )
+  end
+
 end
--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -1,12 +1,13 @@
 class OrderAddress
   include ActiveModel::Model
-  attr_accessor :post_code, :prefectures_id, :municipalities, :address, :building_num, :telephone_num, :item_id, :token, :user_id
+  attr_accessor :post_code, :prefectures_id, :municipalities, :address, :building_num, :telephone_num, :item_id, :user_id, :token
 
   with_options presence: true do
     validates :post_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
     validates :municipalities
     validates :address
     validates :telephone_num, format: {with: /\A\d{10}\z|\A\d{11}\z/ , message: "is invalid."}
+    validates :token
   end
 
   validates :prefectures_id, numericality: { other_than: 1, message: "can't be blank" }
