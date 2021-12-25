bc6edc656652746c9938921bbc9ae60e4ca9d8bc
naramuratomofumi <tomofuminaramura@gmail.com>
2021-12-25 17:40:50 +0900

    エラーメッセージの修正

--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -3,10 +3,10 @@ class OrderAddress
   attr_accessor :post_code, :prefectures_id, :municipalities, :address, :building_num, :telephone_num, :item_id, :user_id, :token
 
   with_options presence: true do
-    validates :post_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
+    validates :post_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Enter it as follows (e.g. 123-4567)"}
     validates :municipalities
     validates :address
-    validates :telephone_num, format: {with: /\A\d{10}\z|\A\d{11}\z/ , message: "is invalid."}
+    validates :telephone_num, format: {with: /\A\d{10}\z|\A\d{11}\z/ , message: "is too short."}
     validates :token
   end
 
